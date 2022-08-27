c38a9ab9ea02fee2645b8e3e2a40f8b8ccdfa91e
Ron <rondrik155@gmail.com>
2022-08-27 18:22:42 -0400

    3 Working, no styling or decimals

--- calculate.js
+++ calculate.js
@@ -71,10 +71,38 @@ function operate(num1,num2,operator){
 }
 
 function evaluateExpression(expression){
-    alert(expression);
     // split on every three values, if theres ever not three values then say not a valid expression
     let expressionArray = [];
     expressionArray = expression.split(' ');
     console.log(expressionArray);
     // got array - now evaluate with bedmas
+    // requirements:
+    // errors: operator at end of array, operator at start of array, no operator in array - reset display and return error
+    // bedmas rules: from left to right: operate on * and / first
+    // then operate on + and - from left to right
+    // * and / pass
+    for(let i = 0; i < expressionArray.length; i++){
+        if(expressionArray[i] === '*' || expressionArray[i] === '/'){
+            let num1 = parseFloat(expressionArray[i-1]);
+            let num2 = parseFloat(expressionArray[i+1]);
+            let operator = expressionArray[i];
+            let result = operate(num1,num2,operator);
+            expressionArray[i-1] = result;
+            expressionArray.splice(i,2);
+            i = 0;
+        }
+    }
+    // + and - pass
+    for(let i = 0; i < expressionArray.length; i++){
+        if(expressionArray[i] === '+' || expressionArray[i] === '-'){
+            let num1 = parseFloat(expressionArray[i-1]);
+            let num2 = parseFloat(expressionArray[i+1]);
+            let operator = expressionArray[i];
+            let result = operate(num1,num2,operator);
+            expressionArray[i-1] = result;
+            expressionArray.splice(i,2);
+            i = 0;
+        }
+    }
+    return expressionArray[0];
 }
